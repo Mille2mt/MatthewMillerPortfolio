9fc759c20a27e437a329add89a67cf4d6074d9aa
Matthew Miller <miller2mt@gmail.com>
2020-07-04 13:03:32 -0400

    added clickable email

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,7 +7,7 @@ const ContactMe = () => (
             <div className='contactInfoList'>
                 <li className='info'>Charleston, South Carolina</li>
                 <li className='info'><a href='tel:5405293771'>540.529.3771</a></li>
-                <li className='info'>Miller2mt@gmail.com</li>     
+                <li className='info'><a href='mailto:Miller2mt@gmail.com'>Miller2mt@gmail.com</a></li>     
             </div>
         </div>
     </div>
